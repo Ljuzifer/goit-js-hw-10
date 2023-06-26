bd1260ccf13cf04a78f0e82a8910b0e1e2713888
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-06-26 03:15:03 +0300

    Loader

--- src/index.js
+++ src/index.js
@@ -4,19 +4,26 @@ import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 // import { renderByOptions, createBreedsList } from './js/select-list';
+new SlimSelect({
+  select: '#single',
+});
 
 refs.catBreedsList.setAttribute('id', 'single');
 refs.loaderRef.insertAdjacentHTML(
   'beforebegin',
   `<div class="load"><span class='load-img'></span></div>`
 );
+const loadImage = document.querySelector('div.load');
 
-// handleLoadingActive();
-createBreedsList();
+handleLoadingActive();
+setTimeout(() => {
+  createBreedsList();
+}, 800);
 
 refs.catBreedsList.addEventListener('change', createCatDescription);
 
 function renderByOptions(base) {
+  handleLoadingDisable();
   const markup = base
     .map(({ id, name }) => {
       return `<option value=${id}>${name}</option>`;
@@ -37,6 +44,7 @@ function createBreedsList() {
 }
 
 function createCatDescription() {
+  handleLoadingActive();
   refs.catInfoRef.innerHTML = '';
   let breedId = refs.catBreedsList.value;
   fetchCatByBreed(breedId).then(data => {
@@ -45,20 +53,29 @@ function createCatDescription() {
 }
 
 function renderDescription(data) {
-  const catDescr = data.map(({ url, breeds }) => {
-    return `<img class="cat-img" src=${url} alt=${breeds[0].name} width="500">
+  setTimeout(() => {
+    const catDescr = data.map(({ url, breeds }) => {
+      return `<img class="cat-img" src=${url} alt=${breeds[0].name} width="400">
       <div class="cat-descr">
         <h2 class="cat-name">${breeds[0].name}</h2>
         <p class="about">${breeds[0].description}</p>
         <p class="tempo">Temperament: ${breeds[0].temperament}</p>
       </div>`;
-  });
+    });
 
-  refs.catInfoRef.insertAdjacentHTML('beforeend', catDescr);
+    refs.catInfoRef.insertAdjacentHTML('beforeend', catDescr);
+    handleLoadingDisable();
+  }, 800);
 }
 
 function handleLoadingActive() {
-  const loadImage = document.querySelector('div.load');
+  refs.catBreedsList.style.visibility = 'hidden';
   refs.loaderRef.classList.add('active');
   loadImage.classList.add('active');
+}
+
+function handleLoadingDisable() {
+  refs.catBreedsList.style.visibility = 'visible';
+  refs.loaderRef.classList.remove('active');
+  loadImage.classList.remove('active');
 }
